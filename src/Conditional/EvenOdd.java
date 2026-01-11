f831b6306ae6fc39c2d8320e7badf589e871c97e
Amit Yadav <ay3106222@gmail.com>
2026-01-11 19:41:43 +0530

    Check Even or Odd Programme

---
+++ src/Conditional/EvenOdd.java
@@ -0,0 +1,23 @@
+package Conditional;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        System.out.print("Enter num: ");
+        Scanner sc=new Scanner(System.in);
+
+
+        int num=sc.nextInt();
+
+        if(num>0){
+            System.out.println("Positive num: "+num);
+        } else if (num==0) {
+            System.out.println("Zero num: "+num);
+        }
+        else {
+            System.out.println("Negative number: "+num);
+        }
+    }
+}
