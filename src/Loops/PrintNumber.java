803d14bb7b4cf0382237829f97cfe1c78677fa5c
Amit Yadav <ay3106222@gmail.com>
2026-01-11 19:23:57 +0530

    Print Numbers from 1 to 10 (for loop)

---
+++ src/Loops/PrintNumber.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class PrintNumber {
+    public static void main(String[] args) {
+        //Print Number Programme using loops
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println(i+" ");
+        }
+    }
+}
