9e1f5b4a05063b5f333f63b13f17a7e79a554ca6
Amit Yadav <ay3106222@gmail.com>
2026-01-11 19:31:47 +0530

    Print Even Numbers from 1 to 20

---
+++ src/Loops/PrintSumNumber.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class PrintSumNumber {
+    public static void main(String[] args) {
+        //Print sum number between 1 to 20
+        for (int i = 1; i <=20 ;i+=2) {
+            System.out.println(i+" ");
+        }
+    }
+}
